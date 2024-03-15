ef40d72157054a938f6b904c43e9cb2f0b042c7d
Daniel Hruška <daniel.hruska@me.com>
2024-03-15 12:35:52 +0100

    Docs to Dist

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     server: { port: 9210 },
-    outDir: './docs',
     site: `https://danosek.github.io/`,
 	base: import.meta.env.DEV
 		? '/'
